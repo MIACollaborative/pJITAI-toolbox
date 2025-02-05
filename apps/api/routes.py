587b1dfa52018292df9013a1999f3d9624ae0ea5
Chris Hong <tippy33@naver.com>
2025-02-05 14:56:33 -0500

    create fake decision, upload data

--- apps/api/routes.py
+++ apps/api/routes.py
@@ -32,6 +32,7 @@ import traceback
 import pandas as pd
 from flask import jsonify, request, render_template
 from flask_login import login_required
+from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 
 from apps import db
@@ -339,11 +340,13 @@ def proj(uuid):
                 covariate_names.append(value)
     base_url = request.host
 
+    max_id = db.session.query(func.max(Decision.id)).scalar()
+
     if not proj:
         return {"status": "error",
                 "message": "Project ID does not exist or project has not been finalized yet."}, 400
     segment = 'main_project_page_finalized'
-    return render_template("design/projects/final_page.html", project_uuid=uuid, proj=proj.as_dict(), covariate_names=covariate_names, segment=segment, time=time, base_url=base_url, token=proj.auth_token)
+    return render_template("design/projects/final_page.html", project_uuid=uuid, proj=proj.as_dict(), covariate_names=covariate_names, segment=segment, time=time, base_url=base_url, token=proj.auth_token, max_id=max_id)
 
 def get_algo_name(uuid):
     proj = db.session.query(Projects).filter(Projects.uuid == uuid).filter(Projects.project_status == 1).first()
